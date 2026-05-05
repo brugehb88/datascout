f9c311ecaf7094d53c79da73d316e1a7575d63ed
Bruno H Borchis de Castro <brunohb88@gmail.com>
2026-05-05 12:05:32 -0300

    fix: update stripe api version

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -2,9 +2,10 @@ import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
+export const maxDuration = 30;
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-11-20",
+  apiVersion: "2026-02-25.clover",
 });
 
 const supabase = createClient(
